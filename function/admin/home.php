c026b0b949807b4697a0005e1222e0daf2b66edd
LENOVO <sikuringaq@gmail.com>
2024-01-27 18:31:03 +0700

    first init github

---
+++ function/admin/home.php
@@ -0,0 +1,5 @@
+<div class="jumbotron text-center">
+  <h1>Selamat Datang Di Halaman Admin</h1>
+  <br><center><p>Repost by <a href='https://stokcoding.com/' title='StokCoding.com' target='_blank'>StokCoding.com</a></p></center>
+  
+</div>
